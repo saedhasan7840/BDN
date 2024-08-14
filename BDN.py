195d65fc5b6fc977aa6d01c7f1a5d4b6a89188da
AK Saed <165506282+saedhasan7840@users.noreply.github.com>
2024-08-14 21:11:20 +0600

    Update BDN.py

--- BDN.py
+++ BDN.py
@@ -9,16 +9,13 @@ from datetime import datetime
 from time import sleep
 from time import sleep as waktu
 try:
-    import pycurl
-    from io import BytesIO
-except:
-    os.system('p'+'ip ins'+'tall pyc'+'url > /dev'+'/null')
-    import pycurl
-    from io import BytesIO
-from string import *
-from concurrent.futures import ThreadPoolExecutor as tred
-from bs4 import BeautifulSoup
-from bs4 import BeautifulSoup as sop
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+    import mechanize
+    from requests.exceptions import ConnectionError
+except ModuleNotFoundError:
+    os.system('pip install mechanize requests futures bs4==2 > /dev/null')
+    os.system('pip install bs4')
 #__________________[ LOOP ]__________________#
 loop = 0
 oks = []
